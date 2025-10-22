1257c01a3bdf03df4efeb8452f601f75ec9b935a
szabogabor8686 <szabogabor8686@gmail.com>
2025-10-22 22:51:21 +0200

    galery ready in mobile view

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,7 @@
 import { addActiveClass, removeActiveClass } from './modules/01-navbar.js';
 import { getMenu, getDrinks } from './modules/03-menu-data.js';
 import { makeDrinksVisible, backToFood } from './modules/03-menu-actions.js';
+import { prevSlide, nextSlide, dotControl } from './modules/04-galery.js';
 
 addActiveClass();
 removeActiveClass();
@@ -8,3 +9,6 @@ getMenu();
 getDrinks();
 makeDrinksVisible();
 backToFood();
+prevSlide();
+nextSlide();
+dotControl();
---
+++ assets/scripts/modules/04-galery.js
@@ -0,0 +1,69 @@
+const slides = document.getElementsByClassName('slide');
+const dots = document.getElementsByClassName('dot');
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+let slideIndex = 1;
+let intervalID;
+let timer;
+
+function showSlides(n) {
+  slideIndex = n;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  if (slideIndex < 1) {
+    slideIndex = slides.length;
+  }
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (let j = 0; j < dots.length; j++) {
+    dots[j].classList.remove('active');
+  }
+  slides[slideIndex - 1].style.display = 'block';
+  dots[slideIndex - 1].classList.add('active');
+}
+
+function restartSlideshow(delay = 3000) {
+  clearTimeout(timer);
+  clearInterval(intervalID);
+  timer = setTimeout(() => {
+    intervalID = setInterval(() => {
+      slideIndex++;
+      showSlides(slideIndex);
+    }, 3000);
+  }, delay);
+}
+
+const prevSlide = () => {
+  prevBtn.addEventListener('click', () => {
+    showSlides(slideIndex - 1);
+    restartSlideshow();
+  });
+};
+
+const nextSlide = () => {
+  nextBtn.addEventListener('click', () => {
+    showSlides(slideIndex + 1);
+    restartSlideshow();
+  });
+};
+
+const dotControl = () => {
+  for (let i = 1; i <= slides.length + 1; i++) {
+    dots[i - 1].addEventListener('click', () => {
+      showSlides(i);
+      restartSlideshow();
+    });
+  }
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  showSlides(slideIndex);
+  intervalID = setInterval(() => {
+    slideIndex++;
+    showSlides(slideIndex);
+  }, 3000);
+});
+
+export { prevSlide, nextSlide, dotControl };
